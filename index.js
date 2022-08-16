71e6b8c153ab9d51c39563286f556227ca17a781
Sonu Kumar <75324160+SimplySonu@users.noreply.github.com>
2022-08-16 12:24:50 +0000

    Add few more question

--- index.js
+++ index.js
@@ -2,41 +2,48 @@ var readlineSync = require("readline-sync");
 
 var score = 0;
 const quiz = [
-	{ question: "Whats my favourite color ? ", answer: "black" },
-	{ question: "Whats my favourite dish ? ", answer: "pizza" },
-	{ question: "Do I like to gym ? ", answer: "yes" },
+  { question: "Whats my favourite color ? ", answer: "black" },
+  { question: "Whats my favourite dish ? ", answer: "pizza" },
+  { question: "Do I like to gym ? ", answer: "yes" },
+  { question: "What's my favourite place ? ", answer: "Manali" }, {
+    question: "Who is my favourite actor? ", answer: "Tom Cruise"
+  },
+  {
+    question: " What is my favourite hobby? ",
+    answer: "Coding"
+  },
 ];
 
 const initialPlayers = { naveen: 3, vishal: 1 };
 
 function getUserName() {
-	var userName = readlineSync.question("May I have your name? ");
-	console.log("Welcome " + userName + "!" + " How much do you know Sonu?");
+  var userName = readlineSync.question("May I have your name? ");
+  console.log("Welcome " + userName + "!" + " How much do you know Sonu?");
 }
 
 function testMyFriend() {
-	for (let i = 0; i < quiz.length; i++) {
-		let answer = readlineSync.question(quiz[i].question);
-		if (answer.toUpperCase() === quiz[i].answer.toUpperCase()) {
-			console.log("You're Right!");
-			score += 1;
-		} else {
-			console.log("Sorry!, You're Wrong");
-		}
-		console.log("Your Current Score is ", score);
-		console.log("------------------");
-	}
+  for (let i = 0; i < quiz.length; i++) {
+    let answer = readlineSync.question(quiz[i].question);
+    if (answer.toUpperCase() === quiz[i].answer.toUpperCase()) {
+      console.log("You're Right!");
+      score += 1;
+    } else {
+      console.log("Sorry!, You're Wrong");
+    }
+    console.log("Your Current Score is ", score);
+    console.log("------------------");
+  }
 }
 
 function checkHighest() {
-	if (score >= initialPlayers.naveen) {
-		console.log("Congralutations! You beat other players");
-	} else {
-		console.log("Your final score is:", score, "\nOther competitors");
-		for (const key in initialPlayers) {
-			console.log(key + ":" + initialPlayers[key]);
-		}
-	}
+  if (score >= initialPlayers.naveen) {
+    console.log("Congralutations! You beat other players");
+  } else {
+    console.log("Your final score is:", score, "\nOther competitors");
+    for (const key in initialPlayers) {
+      console.log(key + ":" + initialPlayers[key]);
+    }
+  }
 }
 
 getUserName();
